e29bd51a4645d43c9dd9ca5822f7c314e3f0286e
Siwani Veerakathy <siwani1155@gmail.com>
2024-03-31 19:23:19 +0200

    verandering teruggedraait ajax

--- menu/menu.php
+++ menu/menu.php
@@ -3,14 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kreta Voorburg | Home</title>
+    <title>Kreta Voorburg | Menu</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="../style.css" rel="stylesheet">
 </head>
 <body>
 
 <?php include('../utensils/header.php'); ?>
-
 
 <div class="container">
     <div class="row">
